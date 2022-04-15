14cf2be7f6553190694f8414ab024a30be9010e0
MrChikip <mrchikip@users.noreply.github.com>
2022-04-14 21:41:30 -0500

    comit inicial
    servidor configurado para empezar a desarrollar las funcionalidades requeridas

---
+++ src/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+//initializations
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//global variables
+app.use((req, res, next) => {
+
+    next();
+});
+
+//routes
+app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+app.use('/links',require('./routes/links'));
+
+//public
+app.use(express.static(path.join(__dirname, 'public')));
+
+//starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
