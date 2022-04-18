3bfc08004fea070f4e0e38932ed0bf3aa9411a19
MrChikip <mrchikip@users.noreply.github.com>
2022-04-17 21:05:28 -0500

    rellenado de los datos no etereos en las base de datos e inicio de la estructura para crud de empleados

---
+++ database/semana.sql
@@ -0,0 +1,7 @@
+INSERT INTO semana VALUES (1, 'lunes');
+INSERT INTO semana VALUES (2, 'martes');
+INSERT INTO semana VALUES (3, 'miercoles');
+INSERT INTO semana VALUES (4, 'jueves');
+INSERT INTO semana VALUES (5, 'viernes');
+INSERT INTO semana VALUES (6, 'sabado');
+INSERT INTO semana VALUES (7, 'domingo');
